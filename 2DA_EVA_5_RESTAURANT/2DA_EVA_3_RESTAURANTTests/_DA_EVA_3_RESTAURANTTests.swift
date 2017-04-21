f63de2f35fca6345cb63f1c3edc3287fecb4a3e8
leohurtado <leonardo.hurtadov@gmail.com>
2017-04-21 17:17:08 -0600

    Add files via upload

---
+++ 2DA_EVA_5_RESTAURANT/2DA_EVA_3_RESTAURANTTests/_DA_EVA_3_RESTAURANTTests.swift
@@ -0,0 +1,36 @@
+//
+//  _DA_EVA_3_RESTAURANTTests.swift
+//  2DA_EVA_3_RESTAURANTTests
+//
+//  Created by LUIS FERNANDO on 08/03/17.
+//  Copyright © 2017 LUIS FERNANDO. All rights reserved.
+//
+
+import XCTest
+@testable import _DA_EVA_3_RESTAURANT
+
+class _DA_EVA_3_RESTAURANTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
